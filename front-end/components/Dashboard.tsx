187d682c606d6210fb75084a0cfe35e099724ae2
Rebecca Scott <me@becdetat.com>
2023-05-14 12:10:35 +1000

    Front-end shell

---
+++ front-end/components/Dashboard.tsx
@@ -0,0 +1,15 @@
+/*
+Main dashboard for the application, when signed in.
+
+What do we need to see here:
+- Edit decision engine rules
+- Set up a new device
+    - get an API key for the device
+    - instructions on how to configure the library on the device
+- traffic stats
+- 
+*/
+
+export const Dashboard = () => {
+    return <p>Dashboard</p>;
+};
